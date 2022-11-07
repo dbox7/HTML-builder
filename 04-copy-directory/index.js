f08f67b88075013f6f91f1108f0d40972d85ddfe
dbox7 <dbox.insight@gmail.com>
2022-11-08 02:13:57 +0300

    feat: add file copying feature

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {if (err) console.log(err)});
+
+fs.readdir(
+    path.join(__dirname, 'files'), 
+    (err, files) => {
+        files.forEach(file => {
+            fs.copyFile(
+                path.join(__dirname, 'files', file), 
+                path.join(__dirname, 'files-copy', file), 
+                err => err,
+            );
+        });
+    }
+);
