fb613efe3c0d3d5ff8f06dcfc28c2ae584182930
dbox7 <dbox.insight@gmail.com>
2023-05-09 12:43:26 +0300

    fix: fix problem with folder updating

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,17 +1,33 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {if (err) console.log(err)});
 
-fs.readdir(
+// fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, err => {console.log(err)});
+// fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {});
+
+fs.access(path.join(__dirname, 'files-copy'), async error => {
+  if (error) {
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {});
+    copyFile();
+  } else {
+    await fs.promises.rm(path.join(__dirname, 'files-copy'), { recursive: true }, err => {console.log(err)});
+    await fs.promises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {});
+    copyFile();
+  }
+})
+
+function copyFile() {
+  fs.readdir(
     path.join(__dirname, 'files'), 
     (err, files) => {
-        files.forEach(file => {
-            fs.copyFile(
-                path.join(__dirname, 'files', file), 
-                path.join(__dirname, 'files-copy', file), 
-                err => err,
-            );
-        });
+      files.forEach(file => {
+        fs.copyFile(
+          path.join(__dirname, 'files', file), 
+          path.join(__dirname, 'files-copy', file), 
+          err => err,
+        );
+      });
     }
-);
+  );
+}
+
